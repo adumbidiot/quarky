1c0588ffcbc289d8498be6eb3abb053c70a84f78
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2025-03-17 19:55:58 -0700

    Add nitter crate support to quarky

--- src/commands/random_tweet.rs
+++ src/commands/random_tweet.rs
@@ -1,10 +1,12 @@
 use crate::{
     CommandContext,
+    NitterClientKey,
     RssClientKey,
 };
 use anyhow::Context as _;
 use log::warn;
 use rand::prelude::IndexedRandom;
+use reqwest::Url;
 use rss_client::RssFeed;
 use std::{
     future::Future,
@@ -48,7 +50,22 @@ async fn get_nitter_feed(
         .with_context(|| format!("failed to get nitter rss feed for \"{user}\" from \"{host}\""))
 }
 
-pub async fn get_random_tweet_url(
+fn extract_id_from_nitter_url<'a>(url: &'a Url, user: &str) -> Option<&'a str> {
+    // https://<domain>/<user>/status/<tweet_id>#m
+
+    let mut path_iter = url.path_segments()?;
+    if path_iter.next()? != user {
+        return None;
+    }
+
+    if path_iter.next()? != "status" {
+        return None;
+    }
+
+    path_iter.next()
+}
+
+async fn get_random_tweet_id_nitter_rss(
     client: &rss_client::Client,
     user: &str,
 ) -> anyhow::Result<Option<String>> {
@@ -63,27 +80,54 @@ pub async fn get_random_tweet_url(
         .channel
         .item
         .iter()
+        .filter_map(|item| extract_id_from_nitter_url(&item.link.link, user))
+        .collect();
+
+    Ok(entries.choose(&mut rand::rng()).map(|v| v.to_string()))
+}
+
+async fn get_random_tweet_id_nitter_scrape(
+    client: &nitter::Client,
+    user: &str,
+) -> anyhow::Result<Option<String>> {
+    let base_url = Url::parse("https://nitter.net/")?;
+
+    let url = format!("https://nitter.net/{user}/media");
+    let feed = client.scrape_feed(&url).await?;
+    let entries: Vec<_> = feed
+        .timeline_items
+        .iter()
         .filter_map(|item| {
-            // https://<domain>/<user>/status/<tweet_id>#m
-
-            let mut path_iter = item.link.link.path_segments()?;
-            if path_iter.next()? != user {
-                return None;
-            }
-
-            if path_iter.next()? != "status" {
-                return None;
-            }
-
-            path_iter.next()
+            let url = Url::options()
+                .base_url(Some(&base_url))
+                .parse(&item.link)
+                .ok()?;
+            extract_id_from_nitter_url(&url, user).map(|v| v.to_string())
         })
         .collect();
 
+    Ok(entries.choose(&mut rand::rng()).cloned())
+}
+
+pub async fn get_random_tweet_url(
+    rss_client: &rss_client::Client,
+    nitter_client: &nitter::Client,
+    user: &str,
+) -> anyhow::Result<Option<String>> {
+    let mut tweet_id_result = Err(anyhow::Error::msg("failed to get random tweet"));
+    if tweet_id_result.is_err() {
+        tweet_id_result = get_random_tweet_id_nitter_rss(rss_client, user).await;
+    }
+    if tweet_id_result.is_err() {
+        tweet_id_result = get_random_tweet_id_nitter_scrape(nitter_client, user).await
+    }
+
+    let maybe_tweet_id = tweet_id_result?;
+
     // Twitter embeds broke as of 10/10/2023.
     // Use https://github.com/FixTweet/FixTweet instead for embedding.
-    let url = entries
-        .choose(&mut rand::rng())
-        .map(|tweet_id| format!("https://fxtwitter.com/{user}/status/{tweet_id}"));
+    let url =
+        maybe_tweet_id.map(|tweet_id| format!("https://fxtwitter.com/{user}/status/{tweet_id}"));
 
     Ok(url)
 }
@@ -94,18 +138,18 @@ pub async fn random_tweet(
     ctx: CommandContext<'_>,
     #[description = "The user"] user: String,
 ) -> anyhow::Result<()> {
-    let rss_client = ctx
-        .serenity_context()
-        .data
-        .read()
-        .await
-        .get::<RssClientKey>()
-        .unwrap()
-        .clone();
+    let rss_client;
+    let nitter_client;
+    {
+        let data = ctx.serenity_context().data.read().await;
+
+        rss_client = data.get::<RssClientKey>().unwrap().clone();
+        nitter_client = data.get::<NitterClientKey>().unwrap().clone();
+    }
 
     ctx.defer().await?;
 
-    match get_random_tweet_url(&rss_client, &user)
+    match get_random_tweet_url(&rss_client, &nitter_client, &user)
         .await
         .with_context(|| format!("failed to get random tweet for \"{user}\""))
         .and_then(|maybe_url| {
